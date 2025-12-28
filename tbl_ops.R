399d46ebf6d71e2d3659a02c5f5bec9838e8c7cb
Caleb <thecalebhwhite@outlook.com>
2025-12-28 12:16:19 -0500

    Add tbl_ops utilities with generalized array-style table operation helper

--- tbl_ops.R
+++ tbl_ops.R
@@ -1,40 +1,64 @@
+#/******************************************************************************
+# PROGRAM NAME: tbl_ops.R
+# PROJECT: Decarbonization Scenarios
+# DESCRIPTION: General-purpose table operations and array-style math utilities
+# DATE: 2025-12-28
+# R-VERSION: R version 4.5.2 (2025-11-01)
+#******************************************************************************/
 
-
-f.arr_op <- function(df_a, df_b, op, 
-                     operand_A, operand_B, join_by_col = "meas") {
+f_arr_op <- function(df_a, 
+                     df_b, 
+                     op, 
+                     operand_a, 
+                     operand_b, 
+                     by = "meas", 
+                     result_col = NULL, 
+                     suffix = c(".a", ".b"), 
+                     keep = c("all", "keys_and_result")) {
   
-  # Define the name of the new results column
-  results_col <- paste(operand_A, operand_B, "result", sep = "_")
-  
-  # Use a named vector for 'by' argument in join
-  matching_indexes <- setNames(join_by_col, join_by_col)
-  
-  # Perform the operation using the appropriate operator function
-  # The 'op' argument should be a function (e.g., `+`, `-`, etc.)
+  keep <- match.arg(keep)
   op_func <- match.fun(op)
   
-  joined_df <- left_join(df_a, df_b, by = matching_indexes) %>% 
-    mutate(
-      # Assign the new column using the `:=` operator for dynamic naming
-      # and the embrace operator {{ to use the string `results_col` as the name
-      "{{results_col}}" := op_func(!!sym(operand_A), !!sym(operand_B)), 
-      # Keep only necessary columns
-      .keep = "unused"
+  if (is.null(result_col)) {
+    result_col <- paste0(operand_a, "_", operand_b, "_result")
+  }
+  
+  joined <- dplyr::left_join(df_a, df_b, by = by, suffix = suffix)
+  
+  # Figure out which column name operand_b has after join
+  # If operand_b exists in df_a too, it will come in as operand_b.b
+  operand_b_joined <- if (operand_b %in% names(df_a)) paste0(
+    operand_b, suffix[2]) else operand_b
+  operand_a_joined <- if (operand_a %in% names(df_b)) paste0(
+    operand_a, suffix[1]) else operand_a
+  
+  out <- joined %>% 
+    dplyr::mutate(
+      "{result_col}" := op_func(.data[[operand_a_joined]], 
+                                .data[[operand_b_joined]])
     )
   
-  return(joined_df)
+  if (keep == "keys_and_result") {
+    out <- out %>% 
+      dplyr::sekect(dplyr::all_of(by), dplyr::all_of(result_col))
+  }
+  
+  out
 }
 
-# For testing: 
-df_a <- data.frame(sector = c("X", "X", "Y", "Y"), 
-                   meas = c("A", "B", "A", "B"), 
-                   count = c(200, 400, 27, 49), 
-                   total_cost = c(200, 800, 27, 98)
-)
-
-df_b <- data.frame(meas = c("A", "B"), 
-                   cost = c(1000, 800), 
-                   total_cost = c(227000, 3600))
-
-f.arr_op(df_a, df_b, '+', 'count', 'cost')
-f.arr_op(df_a, df_b, '*', 'count', 'cost', 'total_cost')
+# df_a <- data.frame(
+#   sector = c("X", "X", "Y", "Y"), 
+#   meas = c("A", "B", "A", "B"), 
+#   count = c(200, 400, 27, 49), 
+#   total_cost = c(200, 800, 27, 98)
+# )
+# 
+# df_b <- data.frame(
+#   meas = c("A", "B"), 
+#   cost = c(1000, 800), 
+#   total_cost = c(227000, 3600)
+# )
+# 
+# f_arr_op(df_a, df_b, '+', 'count', 'cost', by = "meas")
+# f_arr_op(df_a, df_b, '*', 'count', 'cost', by = "meas", 
+#          result_col = "count_x_cost")
